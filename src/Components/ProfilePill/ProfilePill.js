66ddb30c065861f69dfb1b07c96b4bc22bedc0a2
FHK <100160369+FRQ-ALS@users.noreply.github.com>
2022-11-29 18:51:52 +0000

    added profile menu

--- src/Components/ProfilePill/ProfilePill.js
+++ src/Components/ProfilePill/ProfilePill.js
@@ -1,23 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 
-import "./ProfilePill.css"
+import "./ProfilePill.css";
 
+
+const menuItems = ["Profile", "Settings", "Dashboard", "Log out"]
 
 export default function ProfilePill() {
+  const [menuDrawn, setMenuDrawn] = useState(false);
+
+  const handleMenuDraw = (e) => {
+    setMenuDrawn(!menuDrawn);
+  };
 
 
-    return (
-        <button variant="outlined" className="profile-container">
+  function renderMenuItems() {
+
+    return (<div className="menu">
+      {menuItems.map((item) => (
+        <button className="menu-item">{item}</button>
+      ))}
+    </div>);
+  }
+
+  return (
+    <div className="main-container">
+      {menuDrawn ? renderMenuItems() : null}
+      <button
+        onClick={handleMenuDraw}
+        variant="outlined"
+        className="profile-container"
+      >
         <div className="icon">
           <img
             className="icon"
             src="https://i.picsum.photos/id/208/200/200.jpg?hmac=J1BdqRgAAAId9wernbPINrW38haBGOtrpEqn3m2wjlY"
           ></img>
         </div>
-  
-        <h1 className="profile-name">Farooq</h1>
-        </button>
-      );
 
-
+        <h1 className="profile-name">Farooq Al-Saadi</h1>
+      </button>
+    </div>
+  );
 }
