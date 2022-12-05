75d75f5efe0d80ac08a75992d56739ffc51c4f21
FHK <100160369+FRQ-ALS@users.noreply.github.com>
2022-12-05 18:40:40 +0000

    taskbar changes

--- src/Components/ProjectsTaskBar/ProjectTaskBar.js
+++ src/Components/ProjectsTaskBar/ProjectTaskBar.js
@@ -3,22 +3,33 @@ import "./ProjectTaskBar.css";
 import SearchIcon from "@mui/icons-material/Search";
 import AddIcon from "@mui/icons-material/Add";
 
-import AddProjectDialog from "../../AddProjectDialog/AddProjectDialog";
+import AddProjectDialog from "../AddProjectDialog/AddProjectDialog";
 
 export default function ProjectTaskBar() {
   const [dialogOpen, setDialogOpen] = useState(false);
+  const [searchOpen, setSearchOpen] = useState(false)
 
   const handleAddButton = () => {
+    if (dialogOpen === true) return;
+
     setDialogOpen(!dialogOpen);
   };
 
+  const handleSearchButton = () => {
+    setSearchOpen(!searchOpen)
+  };
+
   return (
-    <>
+    <div id="mainContainer">
       <div id="taskbar-container">
-        <SearchIcon className="button" />
+        <SearchIcon onClick={handleSearchButton} id="search" className="true" />
+        {searchOpen ? <input id="input"></input> : null}
         <AddIcon onClick={handleAddButton} className="button" />
       </div>
-      <AddProjectDialog open={dialogOpen} />
-    </>
+      <AddProjectDialog
+        show={dialogOpen}
+        onClickOutside={(event) => setDialogOpen(false)}
+      />
+    </div>
   );
 }
