44a31ef56bc11dc30eb05f21542c10ab2a85883d
FRQ-ALS <farooqalsaadi0@gmail.com>
2022-12-07 14:48:35 +0000

    fixed data mapping issue in projectcard

--- src/Components/JoinProjectCard/JoinProjectCard.js
+++ src/Components/JoinProjectCard/JoinProjectCard.js
@@ -1,12 +1,33 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
+import "./JoinProject.css";
+
+const projects = [1, 2, 3, 4, 5, 6, 7, 8];
+const collaborators = [1, 2, 3, 4];
+
 export default function JoinProjectCard() {
   const params = useParams();
   const jsonArray = JSON.stringify(params);
   const token = JSON.parse(jsonArray).token;
+  const [projectData, setProjectData] = useState("");
+  
+  const taskStatus = [
+    {
+      name: "Pending",
+      value: projectData.pendingTasks,
+    },
+    {
+      name: "Completed",
+      value: projectData.completedTasks,
+    },
+    {
+      name: "Blocked",
+      value: projectData.blockedTasks,
+    },
+  ];
 
-  let jwt = localStorage.getItem("jwt")
+  let jwt = localStorage.getItem("jwt");
 
   useEffect(() => {
     fetch(`api/v1/projects/getProjectByToken/${token}`, {
@@ -16,11 +37,37 @@ export default function JoinProjectCard() {
         "Content-Type": "application/json",
         Authorization: `Bearer ${jwt}`,
       },
-    }).then((response) => response.json())
-    .then((responseJson) => {
-        console.log(responseJson)
-    });
+    })
+      .then((response) => response.json())
+      .then((responseJson) => {
+        console.log(responseJson);
+        setProjectData(responseJson);
+      });
   }, []);
 
-  return <div>{token}</div>;
+  return (
+    <div id="project">
+      <h1 id="heading">{projectData.projectTitle}</h1>
+      <div id="status-container">
+        {taskStatus.map((status, index) => (
+          <div key={index} id="mappedStatus">
+            <div id="status-indicator" className={status.name}>
+              {status.value}
+            </div>
+            <h1 id="task-status-heading">{status.name}</h1>
+          </div>
+        ))}
+      </div>
+      <div id="collaborators">
+        {collaborators.map((user) => (
+          <div id="user">
+            <img
+              id="image"
+              src="https://i.picsum.photos/id/900/200/200.jpg?hmac=ZrAJ9H_K0TLi9qA-7h0aKGGzI3tLtlu1lx6ntCljBfc"
+            ></img>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
